836b0a5101e892cccf5faea8f7204b74e9221a04
Jano-Guerra11 <janoguerra04@gmail.com>
2025-04-16 19:17:33 -0300

    aporte 1

---
+++ src/ejercicio1/Archivo.java
@@ -0,0 +1,19 @@
+package ejercicio1;
+
+
+public class Archivo {
+    private String ruta;
+    
+    public Archivo(String ruta) {
+    	this.ruta = ruta;
+    }
+    
+    public String getRuta() {
+		return ruta;
+	}
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+
+}
