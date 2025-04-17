3d4760d964c519c1a430d016e281ee5938b349bc
liocoma <lionelcorradini@gmail.com>
2025-04-17 20:08:08 -0300

    Clase Persona

---
+++ src/ejercicio1/Persona.java
@@ -0,0 +1,51 @@
+package ejercicio1;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String dni;
+
+    // Constructores
+    public Persona() {
+        this.nombre = "Sin nombre";
+        this.apellido = "Sin apellido";
+        this.dni = "00000000";
+    }
+
+    public Persona(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " " + dni;
+    }
+}
